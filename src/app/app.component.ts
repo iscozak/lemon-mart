7603c256596d34a2c10d4892ed18318473dbef28
isaac <isaac.cobo@gmail.com>
2021-02-16 00:13:57 +0100

    pajina de login y avisos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,43 @@ import { Component } from '@angular/core'
 import { MatIconRegistry } from '@angular/material/icon'
 import { DomSanitizer } from '@angular/platform-browser'
 
+import { AuthService } from './auth/auth.service'
+
 @Component({
   selector: 'app-root',
   template: `
-    <mat-toolbar color="primary" fxLayoutGap="8px">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+    <mat-toolbar
+      color="primary"
+      fxLayoutGap="8px"
+      *ngIf="{
+        status: authService.authStatus$ | async,
+        user: authService.currentUser$ | async
+      } as auth"
+    >
+      <button *ngIf="auth?.status?.isAuthenticated" mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
       <a mat-icon-button routerLink="/home">
         <mat-icon svgIcon="lemon"></mat-icon>
         <span class="mat-h2">LemonMart</span>
       </a>
       <span class="flex-spacer"></span>
       <button
+        *ngIf="auth?.status?.isAuthenticated"
         mat-mini-fab
         routerLink="/user/profile"
         matTooltip="Profile"
         aria-label="User Profile"
       >
-        <mat-icon>account_circle</mat-icon>
+        <img
+          *ngIf="auth?.user?.picture"
+          class="image-cropper"
+          [src]="auth?.user?.picture"
+        />
+        <mat-icon *ngIf="!auth?.user?.picture">account_circle</mat-icon>
       </button>
       <button
+        *ngIf="auth?.status?.isAuthenticated"
         mat-mini-fab
         routerLink="/user/logout"
         matTooltip="Logout"
@@ -31,11 +49,26 @@ import { DomSanitizer } from '@angular/platform-browser'
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+      .image-cropper {
+        width: 40px;
+        height: 40px;
+        position: relative;
+        overflow: hidden;
+        border-radius: 50%;
+        margin-top: -8px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'lemon-mart'
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    public authService: AuthService
+  ) {
     iconRegistry.addSvgIcon(
       'lemon',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,16 +15,20 @@ import { AuthService } from '../auth/auth.service'
     `,
   ],
   template: `
-    <div fxLayout="column" fxLayoutAlign="center center">
-      <span class="mat-display-2">Hello, Limoncu!</span>
-      <button mat-raised-button color="primary" (click)="login()">
-        Login as Manager
-      </button>
+    <div *ngIf="(authService.authStatus$ | async)?.isAuthenticated; else doLogin">
+      <div class="mat-display-4">This is LemonMart! The place where</div>
+      <div class="mat-display-4">
+        You get a lemon, you get a lemon, you get a lemon...
+      </div>
+      <div class="mat-display-4">Everybody gets a lemon.</div>
     </div>
+    <ng-template #doLogin>
+      <app-login></app-login>
+    </ng-template>
   `,
 })
 export class HomeComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
